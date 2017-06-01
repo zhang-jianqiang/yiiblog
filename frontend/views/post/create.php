fa2aec0a30069c70182d46ed902227bc61e44155
zhang-jianqiang <18232587802@126.com>
2017-06-01 22:17:01 +0800

    添加组件 标签图上传组件 百度编辑器组件 标签组件

--- frontend/views/post/create.php
+++ frontend/views/post/create.php
@@ -16,10 +16,23 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="panel-body">
             <?php $form = ActiveForm::begin();?>
             <?=$form->field($model, 'title')->textInput(['maxlength' =>true])?>
-            <?=$form->field($model, 'cat_id')->dropDownList([1 => '你好', 2 => '哈哈'])?>
-            <?=$form->field($model, 'label_img')->textInput(['maxlength' =>true])?>
-            <?=$form->field($model, 'content')->textInput(['maxlength' =>true])?>
-            <?=$form->field($model, 'tags')->textInput(['maxlength' =>true])?>
+            <?=$form->field($model, 'cat_id')->dropDownList($cat)?>
+            <!--上传标签图组件-->
+            <?= $form->field($model, 'label_img')->widget('common\widgets\file_upload\FileUpload',[
+                'config'=>[
+                    //图片上传的一些配置，不写调用默认配置
+                    //'domain_url' => 'http://www.yii-china.com',
+                ]
+            ]) ?>
+            <!--百度编辑器组件-->
+            <?= $form->field($model, 'content')->widget('common\widgets\ueditor\Ueditor',[
+                'options'=>[
+                    //'initialFrameWidth' => 850,
+                    'initialFrameHeight' => 400,
+                ]
+            ]) ?>
+            <!--标签组件-->
+            <?=$form->field($model, 'tags')->widget('common\widgets\tags\TagWidget')?>
             <div class="form-group">
                 <?=Html::submitButton('发布', ['class' => 'btn btn-success'])?>
             </div>
